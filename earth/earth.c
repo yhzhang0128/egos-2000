2e0ad05ab8c48b2fb42bdf5f48dd71f849168d5e
Yunhao Zhang <yz2327@cornell.edu>
2022-01-19 16:29:26 -0500

    Cleanup

--- earth/earth.c
+++ earth/earth.c
@@ -14,16 +14,10 @@
 
 static struct earth earth;
 
-int intr_cnt;
-void timer_handler(int id, void* arg) {
-    intr_cnt++;
-    int local;
-    SUCCESS("Within timer interrupt round #%d, stack @0x%.8x", intr_cnt, &local);
-}
-
 int main() {
     INFO("Start to initialize the earth layer");
 
+    /* Initialize TTY */
     if (tty_init()) {
         ERROR("Failed at initializing the tty device");
         return -1;
@@ -32,6 +26,7 @@ int main() {
     earth.tty_write = tty_write;
     SUCCESS("Finished initializing the tty device");
     
+    /* Initialize disk */
     if (disk_init()) {
         ERROR("Failed at initializing the disk device");
         return -1;
@@ -40,6 +35,7 @@ int main() {
     earth.disk_write = disk_write;
     SUCCESS("Finished initializing the disk device");
 
+    /* Initialize CPU interrupt */
     if (intr_init()) {
         ERROR("Failed at initializing the CPU interrupts");
         return -1;
@@ -48,18 +44,6 @@ int main() {
     earth.intr_disable = intr_disable;
     earth.intr_register = intr_register;
     SUCCESS("Finished initializing the CPU interrupts");
-
-
-    intr_register(TIMER_INTR_ID, timer_handler);
-    intr_enable();
-
-    int cnt = 0;
-    while(1) {
-        if (cnt < intr_cnt) {
-            INFO("Normal program flow with stack variable @0x%.8x", &cnt);
-            cnt++;
-        }
-    }
 
     return 0;
 }
