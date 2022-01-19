b8ece274f545cffd1777250484da2e5f8fe298a0
Yunhao Zhang <yz2327@cornell.edu>
2022-01-19 18:24:58 -0500

    Update mkfs

--- install/mkfs.c
+++ install/mkfs.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
+char* kernel_processes[] = {
+                            "release/grass.elf",
+                            //"release/fs.elf",
+                            //"release/dir.elf",
+                            //"release/shell.elf"
+};
 char buf[1024 * 1024];
+
 
 int main() {
     freopen("disk.img", "w", stdout);
@@ -11,8 +19,34 @@ int main() {
     memset(buf, 0, sizeof(buf));
     write(1, buf, 1024 * 1024);
 
-    /* grass kernel */
+    /* grass kernel processes */
+    int n = sizeof(kernel_processes) / sizeof(char*);
+    if (n > 8) {
+        fprintf(stderr, "[ERROR] more than 8 kernel processes\n");
+        return -1;
+    }
     
+    fprintf(stderr, "[INFO] Loading %d kernel processes\n", n);
+    for (int i = 0; i < n; i++) {
+        struct stat st;
+        stat(kernel_processes[i], &st);
+        fprintf(stderr, "[INFO] Loading %s: %ld bytes\n", kernel_processes[i], st.st_size);
+
+        if (st.st_size > 128 * 1024) {
+            fprintf(stderr, "[ERROR] file larger than 128KB\n");
+            return -1;
+        }
+
+        int size = 0;
+        freopen(kernel_processes[i], "r", stdin);
+        for (; size < st.st_size; size += read(0, buf, 128 * 1024));
+        write(1, buf, st.st_size);
+        write(1, buf, 128 * 1024 - st.st_size);
+    }
+
+    for (int i = 0; i < 8 - n; i++)
+        write(1, buf, 128 * 1024);
+        
     /* file system */
     
     fclose(stdout);
