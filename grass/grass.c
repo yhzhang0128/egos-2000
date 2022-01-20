3b8e6e73732470d2421f02d20183cb7f1c468569
Yunhao Zhang <yz2327@cornell.edu>
2022-01-19 21:41:54 -0500

    minor

--- grass/grass.c
+++ grass/grass.c
@@ -13,19 +13,10 @@
 
 struct earth *earth = (void*)EARTH_ADDR;
 
-int global_var1;
-int global_var2;
-int global_var3 = 1;
-
 int main() {
-    INFO("Enter the grass layer");
-    
-    global_var1++;
-    global_var2++;
-    global_var3++;
-
+    SUCCESS("Enter the grass layer");
     char* buf = malloc(512);
     free(buf);
-    SUCCESS("stack variable @0x%.8x and heap variable @0x%.8x", &buf, buf);
+    INFO("stack variable @0x%.8x and heap variable @0x%.8x", &buf, buf);
     return 0;
 }
