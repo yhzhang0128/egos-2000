ea9f56dd7ac8cdaf8e68602ec2e8c3cf426e6800
Yunhao Zhang <yz2327@cornell.edu>
2022-01-20 09:42:19 -0500

    minor

--- shared/include/mmu.h
+++ shared/include/mmu.h
@@ -1,12 +1,16 @@
 #pragma once
 
 #define PAGE_SIZE       4096
+
+#define MAX_NPAGES      12
 #define MAX_NFRAMES     100
 #define CACHED_NFRAMES  20
-#define CACHE_START     0x8000C000
 
 #define VADDR_START     0x80000000
-#define MAX_NPAGES      12
+#define VADDR_SIZE      VADDR_START + PAGE_SIZE * MAX_NPAGES
+
+#define CACHE_START     0x8000C000
+#define CACHE_SIZE      CACHE_START + PAGE_SIZE * CACHED_NFRAMES
 
 struct frame {
     char content[PAGE_SIZE];
