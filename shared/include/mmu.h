44277af9a109b39dd2755cd5ebb3d56aea77eb40
Yunhao Zhang <yz2327@cornell.edu>
2022-01-21 16:52:58 -0500

    Unify address style and fix a bug of translation table address

--- shared/include/mmu.h
+++ shared/include/mmu.h
@@ -1,16 +1,21 @@
 #pragma once
 
-#define PAGE_SIZE       4096
+#define PAGE_SIZE           4096
 
-#define MAX_NPAGES      12
-#define MAX_NFRAMES     256
-#define CACHED_NFRAMES  20
+#define MAX_NPAGES          12
+#define MAX_NFRAMES         256
+#define CACHED_NFRAMES      20
 
-#define VADDR_START     0x80000000
-#define VADDR_SIZE      PAGE_SIZE * MAX_NPAGES
+#define VADDR_START         0x80000000
+#define VADDR_SIZE          PAGE_SIZE * MAX_NPAGES
 
-#define CACHE_START     0x8000C000
-#define CACHE_SIZE      PAGE_SIZE * CACHED_NFRAMES
+#define CACHE_START         0x8000C000
+#define CACHE_SIZE          PAGE_SIZE * CACHED_NFRAMES
+
+#define TRANS_TABLE_START   0x08003000
+#define TRANS_TABLE_TOP     0x08004000
+#define TRANS_TABLE_SIZE    sizeof(struct translation_table_t)
+
 
 struct frame_t {
     char byte[PAGE_SIZE];
@@ -24,6 +29,3 @@ struct translation_table_t {
     } frame[MAX_NFRAMES];
 };
 
-#define TRANS_TABLE_START      0x80003000
-#define TRANS_TABLE_TOP        0x80004000
-#define TRANS_TABLE_SIZE       sizeof(struct translation_table_t)
