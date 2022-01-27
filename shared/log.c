53f6eefa9c0ae1b734420227bd34de6df865627f
Yunhao Zhang <yz2327@cornell.edu>
2022-01-26 22:31:36 -0500

    minor

--- shared/log.c
+++ shared/log.c
@@ -10,10 +10,10 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
+#define KNRM  "\x1B[1;0m"
+#define KRED  "\x1B[1;31m"
+#define KGRN  "\x1B[1;32m"
+#define KYEL  "\x1B[1;33m"
 
 int INFO(const char *format, ...)
 {
