17f43834de0b7dc8acafe1d4e3c5ee7f8ac22cd4
Yunhao Zhang <yz2327@cornell.edu>
2022-01-27 09:27:56 -0500

    Rename

--- install/arty_board/write_cfgmem.tcl
+++
@@ -1,1 +0,0 @@
-write_cfgmem -format mcs -interface spix4 -size 16 -loadbit {up 0x0 arty_board/fe310_arty.bit} -loaddata {up 0x400000 earth.bin up 0x800000 disk.img} -file egos_bootROM.mcs -force
--- install/mkrom.c
+++ install/mkrom.c
@@ -21,7 +21,7 @@
 
 char disk_file[]  = "disk.img";
 char earth_file[] = "earth.bin";
-char fe310_file[] = "arty_board/fe310_arty.bit";
+char fe310_file[] = "arty_board/fe310_cpu.bit";
 char output_file[]= "egos_bootROM.mcs";
 
 char mem_fe310[4 * 1024 * 1024];
