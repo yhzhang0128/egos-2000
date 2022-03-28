5cb47cbdd46d8b65a016b80145b56f46e84dbd8d
Yunhao <yz2327@cornell.edu>
2022-03-28 13:28:09 -0400

    cleanup

--- apps/clock.c
+++ apps/clock.c
@@ -10,14 +10,9 @@
 #include "app.h"
 
 int main(int argc, char** argv) {
-    if (argc == 1) {
-        INFO("Usage: clock [name]");
-        return 0;
-    }
-
     volatile int i, cnt = 0;
     while (1) {
         for (i = 0; i < 5000000; i++);
-        printf("clock %s: tick %d\r\n", argv[1], ++cnt);
+        printf("\r\nclock: tick#%d\r\n", argv[1], ++cnt);
     }
 }
