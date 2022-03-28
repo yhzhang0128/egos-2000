5c1c0caa6e478d07de2ef829a781589bbd68c1ac
Yunhao Zhang <yz2327@cornell.edu>
2022-03-28 18:53:55 -0400

    Fix bug

--- apps/sys_proc.c
+++ apps/sys_proc.c
@@ -56,10 +56,12 @@ int main(struct pcb_intf* _pcb) {
             sys_send(GPID_SHELL, (void*)reply, sizeof(reply));
         } else if (req->type == PROC_EXIT) {
             pcb.proc_free(sender);
-            if (shell_waiting)
+            if (shell_waiting && app_pid == sender) {
                 sys_send(GPID_SHELL, (void*)reply, sizeof(reply));
-            else
+            } else {
                 INFO("background process %d terminated", sender);
+            }
+                
         } else if (req->type == PROC_KILLALL){
             pcb.proc_free(-1);
         } else {
