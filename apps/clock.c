f68ee0b93332c6e65dc2707168de3312a062dcf5
Yunhao Zhang <yz2327@cornell.edu>
2022-03-28 19:12:45 -0400

    minor

--- apps/clock.c
+++ apps/clock.c
@@ -8,11 +8,12 @@
  */
 
 #include "app.h"
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    volatile int i, cnt = 0;
-    while (1) {
-        for (i = 0; i < 5000000; i++);
-        printf("clock: tick#%d\r\n", ++cnt);
+    volatile int i, j, cnt = (argc == 1)? 10000 : atoi(argv[1]);
+    for (i = 0; i < cnt; i++) {
+        for (j = 0; j < 5000000; j++);
+        printf("clock: tick#%d\r\n", i + 1);
     }
 }
