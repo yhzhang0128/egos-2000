aedc872d4f9d21d083ff09a2d4902d2e0e06392a
Yunhao Zhang <yz2327@cornell.edu>
2022-03-29 00:37:56 -0400

    minor

--- apps/clock.c
+++ apps/clock.c
@@ -12,6 +12,7 @@
 
 int main(int argc, char** argv) {
     volatile int i, j, cnt = (argc == 1)? 100 : atoi(argv[1]);
+    
     for (i = 0; i < cnt; i++) {
         for (j = 0; j < 5000000; j++);
         printf("clock: tick#%d / #%d\r\n", i + 1, cnt);
