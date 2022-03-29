af0f238c97d11b284b356694d212c5c938b79ab5
Yunhao <yz2327@cornell.edu>
2022-03-29 15:19:12 -0400

    minor

--- earth/dev_log.c
+++ earth/dev_log.c
@@ -10,11 +10,10 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-#define COLOR_RESET  "\x1B[1;0m"
-#define VPRINTF      va_list args; \
-                     va_start(args, format); \
-                     vprintf(format, args); \
-                     va_end(args); \
+#define VPRINTF   va_list args; \
+                  va_start(args, format); \
+                  vprintf(format, args); \
+                  va_end(args); \
 
 int log_info(const char *format, ...)
 {
@@ -25,23 +24,22 @@ int log_info(const char *format, ...)
 
 int log_highlight(const char *format, ...)
 {
-    printf("%s[HIGHLIGHT] ", "\x1B[1;33m");  // yellow
+    printf("%s[HIGHLIGHT] ", "\x1B[1;33m"); // yellow
     VPRINTF
-    printf("%s\r\n", COLOR_RESET);
+    printf("%s\r\n", "\x1B[1;0m");
 }
 
 int log_success(const char *format, ...)
 {
-    printf("%s[SUCCESS] ", "\x1B[1;32m");  // green
+    printf("%s[SUCCESS] ", "\x1B[1;32m");   // green
     VPRINTF
-    printf("%s\r\n", COLOR_RESET);
+    printf("%s\r\n", "\x1B[1;0m");
 }
 
 int log_fatal(const char *format, ...)
 {
-    printf("%s[FATAL] ", "\x1B[1;31m"); // red
+    printf("%s[FATAL] ", "\x1B[1;31m");     // red
     VPRINTF
-    printf("%s\r\n", COLOR_RESET);
+    printf("%s\r\n", "\x1B[1;0m");
     while(1);
 }
-
