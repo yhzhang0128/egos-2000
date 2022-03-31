734a145c991a96f56e8ceda0a6b16242b7bd4cf8
Yunhao Zhang <yz2327@cornell.edu>
2022-03-31 12:43:32 -0400

    cleanup

--- earth/sd/sd.h
+++ earth/sd/sd.h
@@ -4,7 +4,6 @@
 
 /* SD card library interface */
 #define SD_BLOCK_SIZE 512
-#define uint32_t unsigned int
 
 enum {
       SD_CARD_TYPE_SD1,
@@ -15,8 +14,8 @@ enum {
 extern int SD_CARD_TYPE;
 
 int sdinit();
-int sdread(uint32_t offset, uint32_t nblock, char* dst);
-int sdwrite(uint32_t offset, uint32_t nblock, char* src);
+int sdread(int offset, int nblock, char* dst);
+int sdwrite(int offset, int nblock, char* src);
 
 char sd_exec_cmd(char*);
 char sd_exec_acmd(char*);
