e720b8e6eccb453e7c223082a1176b94a9f7b8c6
Yunhao <yz2327@cornell.edu>
2022-04-05 15:38:49 -0400

    cleanup

--- earth/bus_uart.c
+++ earth/bus_uart.c
@@ -8,6 +8,12 @@
  * see chapter18 of the SiFive FE310-G002 Manual
  */
 
+#define GPIO0_BASE    0x10012000UL
+#define GPIO0_IOF_EN  56UL
+
+#define GPIO_REG(offset) (GPIO0_BASE + offset)
+#define GPIO_REGW(offset) (ACCESS((unsigned int*)GPIO_REG(offset)))
+
 #define UART0_BASE    0x10013000UL
 #define UART0_TXDATA  0UL
 #define UART0_RXDATA  4UL
@@ -19,12 +25,6 @@
 #define UART_REG(offset) (UART0_BASE + offset)
 #define UART_REGW(offset) (ACCESS((unsigned int*)UART_REG(offset)))
 
-#define GPIO0_BASE    0x10012000UL
-#define GPIO0_IOF_EN  56UL
-#define GPIO0_IOF_SEL 60UL
-
-#define GPIO_REG(offset) (GPIO0_BASE + offset)
-#define GPIO_REGW(offset) (ACCESS((unsigned int*)GPIO_REG(offset)))
 
 void uart_init(long baud_rate) {
     UART_REGW(UART0_DIV) = CPU_CLOCK_RATE / baud_rate - 1;
@@ -32,7 +32,6 @@ void uart_init(long baud_rate) {
     UART_REGW(UART0_RXCTRL) |= 1;
 
     /* UART0 maps to GPIO pin16 and pin17 on FE310 */
-    GPIO_REGW(GPIO0_IOF_SEL) |= 0;
     GPIO_REGW(GPIO0_IOF_EN) |= 0x30000;
 }
 
