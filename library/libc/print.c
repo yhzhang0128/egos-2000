0852a27ba9807ef7572dd293c90a26c3a7752d7e
Yunhao <yz2327@cornell.edu>
2022-04-06 21:06:12 -0400

    cleanup

--- library/libc/print.c
+++ library/libc/print.c
@@ -10,6 +10,7 @@
 #include "egos.h"
 #include <unistd.h>
 
+/* printf() constructs a string based on its arguments and call _write */
 int _write(int file, char *ptr, int len) {
     if (file != STDOUT_FILENO) return -1;
     return earth->tty_write(ptr, len);
