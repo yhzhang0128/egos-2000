114657dbcd8e7ed3a0658ba320b827be5e9b1fea
Yunhao Zhang <yz2327@cornell.edu>
2023-09-28 23:15:15 -0400

    Fix bug in timer code

--- earth/cpu_timer.c
+++ earth/cpu_timer.c
@@ -12,24 +12,24 @@
 
 #include "egos.h"
 
-static long long mtime_get() {
-    int low, high;
+static unsigned long long mtime_get() {
+    unsigned int low, high;
     do {
-        high = *(int*)(0x200bff8 + 4);
-        low  = *(int*)(0x200bff8);
-    }  while ( *(int*)(0x200bff8 + 4) != high );
+        high = REGW(0x200BFF8, 4);
+        low  = REGW(0x200BFF8, 0);
+    }  while ( REGW(0x200BFF8, 4) != high );
 
-    return (((long long)high) << 32) | low;
+    return (((unsigned long long)high) << 32) | low;
 }
 
-static void mtimecmp_set(long long time) {
-    *(int*)(0x2004000 + 4) = 0xFFFFFFFF;
-    *(int*)(0x2004000 + 0) = (int)time;
-    *(int*)(0x2004000 + 4) = (int)(time >> 32);
+static void mtimecmp_set(unsigned long long time) {
+    REGW(0x2004000, 4) = 0xFFFFFFFF;
+    REGW(0x2004000, 0) = (unsigned int)time;
+    REGW(0x2004000, 4) = (unsigned int)(time >> 32);
 }
 
 int timer_reset() {
-    mtimecmp_set(mtime_get() + 50000);
+    mtimecmp_set(mtime_get() + 500000);
     return 0;
 }
 
