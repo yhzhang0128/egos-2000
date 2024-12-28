3de19e48069a08a968ef9e2ca60cf2daa2028d6e
Yunhao Zhang <yz2327@cornell.edu>
2024-12-28 10:51:48 -0600

    Use emoji

--- library/egos.h
+++ library/egos.h
@@ -49,17 +49,17 @@ extern struct grass* grass;
 
 /* Memory regions */
 #define PAGE_SIZE         4096
-#define RAM_END           0x81000000 /* 16MB memory in total      */
-#define APPS_PAGES_BASE   0x80800000 /* 8MB free for mmu_alloc    */
-#define APPS_STACK_TOP    0x80800000 /* 2MB app stack             */
-#define SYSCALL_ARG       0x80601000 /* struct syscall            */
-#define APPS_ARG          0x80600000 /* main() argc/argv          */
-#define APPS_ENTRY        0x80400000 /* 2MB app code and data     */
-#define EGOS_STACK_TOP    0x80400000 /* 2MB egos stack            */
-#define GRASS_STRUCT_BASE 0x80201000 /* struct grass              */
-#define EARTH_STRUCT_BASE 0x80200000 /* struct earth              */
-#define RAM_START         0x80000000 /* 2MB egos code and data    */
-#define BOARD_FLASH_ROM   0x20400000 /* 4MB disk image on the Arty board ROM */
+#define RAM_END           0x81000000 /* 16MB memory in total     */
+#define APPS_PAGES_BASE   0x80800000 /* 8MB⬆️ free for mmu_alloc */
+#define APPS_STACK_TOP    0x80800000 /* 2MB⬇️ app stack          */
+#define SYSCALL_ARG       0x80601000 /* struct syscall           */
+#define APPS_ARG          0x80600000 /* main() argc/argv         */
+#define APPS_ENTRY        0x80400000 /* 2MB⬆️ app code and data  */
+#define EGOS_STACK_TOP    0x80400000 /* 2MB⬇️ egos stack         */
+#define GRASS_STRUCT_BASE 0x80201000 /* struct grass             */
+#define EARTH_STRUCT_BASE 0x80200000 /* struct earth             */
+#define RAM_START         0x80000000 /* 2MB⬆️ egos code and data */
+#define BOARD_FLASH_ROM   0x20400000 /* 4MB⬆️ disk image on the Arty board ROM */
 
 /* Memory-mapped I/O regions */
 #define ETHMAC_CSR_BASE  0xF0002000
