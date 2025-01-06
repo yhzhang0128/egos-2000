671706ff9b3b19c8c4244e6491123e17f7462f6b
Yunhao Zhang <zhangyunh@gmail.com>
2025-01-05 23:54:47 -0600

    Rename spi_transfer to spi_exchange

--- earth/dev_disk.c
+++ earth/dev_disk.c
@@ -22,7 +22,7 @@
 #define SIFIVE_SPI_RXDATA 76UL
 
 /* Two helper functions for the SPI bus */
-char spi_transfer(char byte) {
+char spi_exchange(char byte) {
     /* "transfer" means sending a byte and then receiving a byte */
     uint rxdata;
     if (earth->platform == ARTY) {
@@ -46,20 +46,20 @@ void spi_set_clock(uint freq) {
 
 /* Send SD card commands through the SPI bus */
 static char sd_exec_cmd(char* cmd) {
-    for (uint i = 0; i < 6; i++) spi_transfer(cmd[i]);
+    for (uint i = 0; i < 6; i++) spi_exchange(cmd[i]);
 
     for (uint reply, i = 0; i < 8000; i++)
-        if ((reply = spi_transfer(0xFF)) != 0xFF) return reply;
+        if ((reply = spi_exchange(0xFF)) != 0xFF) return reply;
 
     return 0xFF;
 }
 
 static char sd_exec_acmd(char* cmd) {
     char cmd55[] = {0x77, 0x00, 0x00, 0x00, 0x00, 0xFF};
-    while (spi_transfer(0xFF) != 0xFF);
+    while (spi_exchange(0xFF) != 0xFF);
     sd_exec_cmd(cmd55);
 
-    while (spi_transfer(0xFF) != 0xFF);
+    while (spi_exchange(0xFF) != 0xFF);
     return sd_exec_cmd(cmd);
 }
 
@@ -70,7 +70,7 @@ static void sd_read(uint offset, char* dst) {
     if (earth->platform == QEMU) offset *= BLOCK_SIZE;
 
     /* Wait until SD card is not busy */
-    while (spi_transfer(0xFF) != 0xFF);
+    while (spi_exchange(0xFF) != 0xFF);
 
     /* Send read request with cmd17 */
     char* arg = (void*)&offset;
@@ -80,10 +80,10 @@ static void sd_read(uint offset, char* dst) {
         FATAL("SD card replies cmd17 with status 0x%.2x", reply);
 
     /* Wait for the data packet and ignore the 2-byte checksum */
-    while (spi_transfer(0xFF) != 0xFE);
-    for (uint i = 0; i < BLOCK_SIZE; i++) dst[i] = spi_transfer(0xFF);
-    spi_transfer(0xFF);
-    spi_transfer(0xFF);
+    while (spi_exchange(0xFF) != 0xFE);
+    for (uint i = 0; i < BLOCK_SIZE; i++) dst[i] = spi_exchange(0xFF);
+    spi_exchange(0xFF);
+    spi_exchange(0xFF);
 }
 
 static void sd_write(uint offset, char* src) {
@@ -92,7 +92,7 @@ static void sd_write(uint offset, char* src) {
     if (earth->platform == QEMU) offset *= BLOCK_SIZE;
 
     /* Wait until SD card is not busy */
-    while (spi_transfer(0xFF) != 0xFF);
+    while (spi_exchange(0xFF) != 0xFF);
 
     /* Send write request with cmd24 */
     char* arg = (void*)&offset;
@@ -101,16 +101,16 @@ static void sd_write(uint offset, char* src) {
         FATAL("SD card replies cmd24 with status %.2x", reply);
 
     /* Transfer 1-byte buffer before writing block */
-    spi_transfer(0xFF);
+    spi_exchange(0xFF);
 
     /* Send data packet: token + block + dummy 2-byte checksum */
-    spi_transfer(0xFE);
-    for (uint i = 0; i < BLOCK_SIZE; i++) spi_transfer(src[i]);
-    spi_transfer(0xFF);
-    spi_transfer(0xFF);
+    spi_exchange(0xFE);
+    for (uint i = 0; i < BLOCK_SIZE; i++) spi_exchange(src[i]);
+    spi_exchange(0xFF);
+    spi_exchange(0xFF);
 
     /* Wait for SD card ack of data packet */
-    while ((reply = spi_transfer(0xFF)) == 0xFF);
+    while ((reply = spi_exchange(0xFF)) == 0xFF);
     if ((reply & 0x1F) != 0x05)
         FATAL("SD card write ack with status 0x%.2x", reply);
 }
@@ -123,19 +123,19 @@ static int sd_init() {
     if (earth->platform == ARTY) {
         spi_set_clock(400000);
         REGW(SPI_BASE, LITEX_SPI_CS) = 0;
-        for (uint i = 0; i < 1000; i++) spi_transfer(0xFF);
+        for (uint i = 0; i < 1000; i++) spi_exchange(0xFF);
         REGW(SPI_BASE, LITEX_SPI_CS) = 1;
     } else {
         REGW(SPI_BASE, SIFIVE_SPI_CSMODE) = 1;
-        for (uint i = 0; i < 1000; i++) spi_transfer(0xFF);
+        for (uint i = 0; i < 1000; i++) spi_exchange(0xFF);
         REGW(SPI_BASE, SIFIVE_SPI_CSDEF) = 1;
     }
 
     INFO("Set the CS pin to LOW and send cmd0 to SD card");
     char reply, cmd0[] = {0x40, 0x00, 0x00, 0x00, 0x00, 0x95};
     if ((reply = sd_exec_cmd(cmd0)) == 0xFF) return -1;
-    while (reply != 0x01) reply = spi_transfer(0xFF);
-    while (spi_transfer(0xFF) != 0xFF);
+    while (reply != 0x01) reply = spi_exchange(0xFF);
+    while (spi_exchange(0xFF) != 0xFF);
 
     INFO("Check SD card type and voltage with cmd8");
     char cmd8[] = {0x48, 0x00, 0x00, 0x01, 0xAA, 0x87};
@@ -148,16 +148,16 @@ static int sd_init() {
         /* Only need the last byte of the r7 response */
         uint payload;
         for (uint i = 0; i < 4; i++)
-            ((char*)&payload)[3 - i] = spi_transfer(0xFF);
+            ((char*)&payload)[3 - i] = spi_exchange(0xFF);
         INFO("SD card replies cmd8 with status %d", reply);
 
         if ((payload & 0xFFF) != 0x1AA) FATAL("Fail to check SD card type");
     }
-    while (spi_transfer(0xFF) != 0xFF);
+    while (spi_exchange(0xFF) != 0xFF);
 
     char acmd41[] = {0x69, 0x40, 0x00, 0x00, 0x00, 0xFF};
     while (sd_exec_acmd(acmd41));
-    while (spi_transfer(0xFF) != 0xFF);
+    while (spi_exchange(0xFF) != 0xFF);
 
     if (earth->platform == ARTY) {
         INFO("Set the SPI clock to 20MHz for the SD card");
