57f090cc6cb90477dbc0c0ef5e1db4f9e69d4de4
Yunhao Zhang <yz2327@cornell.edu>
2025-12-24 08:28:14 -0600

    Start to work on WiFi over ESP32 Pmod

---
+++ apps/user/tcp_demo.c
@@ -0,0 +1,67 @@
+/*
+ * (C) 2025, Cornell University
+ * All rights reserved.
+ *
+ * Description: a simple TCP demo over WiFi
+ */
+
+#include "app.h"
+
+#define ESP32_BASE    0xF0002000UL /* TODO: this conflicts with NIC_BASE */
+#define ESP32_TXFULL  REGW(ESP32_BASE, 4UL)
+#define ESP32_RXEMPTY REGW(ESP32_BASE, 8UL)
+#define ESP32_EVPEND  REGW(ESP32_BASE, 16UL)
+
+void esp32_getc(char* c) {
+    while (ESP32_RXEMPTY);
+    *c           = REGW(ESP32_BASE, 0) & 0xFF;
+    ESP32_EVPEND = 2;
+}
+
+void esp32_putc(char c) {
+    while (ESP32_TXFULL);
+    REGW(ESP32_BASE, 0) = c;
+    ESP32_EVPEND        = 1;
+}
+
+char rep[4096];
+void esp32_get_reply(){
+    memset(rep, 0, 4096);
+    for (int i = 0; i == 0 || rep[i - 1] != '\n'; i++) {
+        esp32_getc(rep + i);
+        //INFO("getc: %d", rep[i]);
+    }
+}
+
+int main() {
+    CRITICAL("Press the button on Pmod ESP32");
+    while (strcmp(rep, "ready\r\n") != 0) esp32_get_reply();
+
+    /* INFO("Reset the ESP32 device"); */
+    /* const char* rst_cmd = "AT+RST\r\n"; */
+    /* for (int i = 0; i < 8; i++) esp32_putc(rst_cmd[i]); */
+    /* do { */
+    /*     esp32_get_reply(); */
+    /*     if (rep[0] != '\r') printf("Get ESP32 reply=%s", rep); */
+    /* } while (strcmp(rep, "ready\r\n") != 0); */
+
+    INFO("Set ESP32 to WiFi Station mode");
+    const char* mode_cmd = "AT+CWMODE=1\r\n";
+    for (int i = 0; i < 13; i++) esp32_putc(mode_cmd[i]);
+    do {
+        esp32_get_reply();
+        if (rep[0] != '\r') printf("Get ESP32 reply=%s", rep);
+    } while (strcmp(rep, "OK\r\n") != 0);
+
+
+    INFO("Connect to WiFi");
+    const char* wifi_cmd = "AT+CWJAP=\"3602\",\"yunhao0128\"\r\n";
+    for (int i = 0; i < 30; i++) esp32_putc(wifi_cmd[i]);
+
+    do {
+        esp32_get_reply();
+        if (rep[0] != '\r') printf("Get ESP32 reply=%s", rep);
+    } while (strcmp(rep, "OK\r\n") != 0);
+
+    return 0;
+}
