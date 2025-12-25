c25883eeb4cbb2d8fac562b008cb334925fd1981
Yunhao Zhang <yz2327@cornell.edu>
2025-12-24 18:41:02 -0600

    Cleanup

--- apps/user/tcp_demo.c
+++ apps/user/tcp_demo.c
@@ -7,56 +7,48 @@
 
 #include "app.h"
 
-#define ESP32_BASE    0xF0002000UL /* TODO: this conflicts with NIC_BASE */
-#define ESP32_TXFULL  REGW(ESP32_BASE, 4UL)
-#define ESP32_RXEMPTY REGW(ESP32_BASE, 8UL)
-#define ESP32_EVPEND  REGW(ESP32_BASE, 16UL)
+#define ESP32_BASE 0xF0002000UL /* TODO: this conflicts with NIC_BASE */
 
-void esp32_getc(char* c) {
-    while (ESP32_RXEMPTY);
-    *c           = REGW(ESP32_BASE, 0) & 0xFF;
-    ESP32_EVPEND = 2;
-}
-
-void esp32_putc(char c) {
-    while (ESP32_TXFULL);
-    REGW(ESP32_BASE, 0) = c;
-    ESP32_EVPEND        = 1;
-}
-
-void esp32_wait(const char* ack) {
+void esp32_wait(const char* ack, int print) {
     char rep[1024] = {0};
     for (int i = 0; i < 1024; i++) {
+        /* Stop if we get the ack string from ESP32. */
         if (i >= strlen(ack) && strcmp(rep + i - strlen(ack), ack) == 0) break;
-        esp32_getc(rep + i);
+        /* Get one more char from ESP32. */
+        while (REGW(ESP32_BASE, 8UL));
+        rep[i]                 = REGW(ESP32_BASE, 0) & 0xFF;
+        REGW(ESP32_BASE, 16UL) = 2;
     }
-    printf("# Get ESP32 reply:\n\r%s", rep);
+    if (print) printf("\x1B[1;32mESP32 runs AP command:\x1B[1;0m\n\r%s", rep);
 }
 
 int main() {
     CRITICAL("Press the button on Pmod ESP32");
-    esp32_wait("ready\r\n");
+    esp32_wait("ready\r\n", 0);
 
-    /* AT+CWMODE   sets ESP32 to WiFi station mode (mode=1)
-     * AT+CWJAP    connects to a WiFi with a password
-     * AT+CIPSTART establishes a TCP connection
-     * AT+CIPSEND  tells ESP32 the length of string hello
+    /* This app runs 4 AP commands on ESP32:
+     *     AT+CWMODE   sets ESP32 to the WiFi station mode;
+     *     AT+CWJAP    connects to a WiFi with a password;
+     *     AT+CIPSTART establishes a TCP connection;
+     *     AT+CIPSEND  asks ESP32 to send a 23-byte string.
      *
-     * Read this document for more details of AP commands:
+     * Read this document for more details on AP commands:
      * http://espressif.com/sites/default/files/documentation/esp32_at_instruction_set_and_examples_en.pdf
      */
-    char* AP_cmds[] = {
-        "AT+CWMODE=1\r\n", "AT+CWJAP=\"3602\",\"yunhao0128\"\r\n",
-        "AT+CIPSTART=\"TCP\",\"192.168.0.212\",8002\r\n", "AT+CIPSEND=23\r\n"};
+    char* AP_cmds[] = {"AT+CWMODE=1\r\n",
+                       "AT+CWJAP=\"3602\",\"yunhao0128\"\r\n",
+                       "AT+CIPSTART=\"TCP\",\"192.168.0.212\",8002\r\n",
+                       "AT+CIPSEND=23\r\n", "Hello from egos-2000!\n\r"};
 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < strlen(AP_cmds[i]); j++) esp32_putc(AP_cmds[i][j]);
-        esp32_wait("OK\r\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < strlen(AP_cmds[i]); j++) {
+            while (REGW(ESP32_BASE, 4UL));
+            REGW(ESP32_BASE, 0)    = AP_cmds[i][j];
+            REGW(ESP32_BASE, 16UL) = 1;
+        }
+
+        if (i != 4) esp32_wait("OK\r\n", 1);
     }
-
-    /* Send the hello string through the TCP connection over WiFi. */
-    char* hello = "Hello from egos-2000!\n\r";
-    for (int i = 0; i < strlen(hello); i++) esp32_putc(hello[i]);
 
     return 0;
 }
