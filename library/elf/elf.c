b39823e4aef6e9d68daa1a302817056362e4e5dc
Yunhao Zhang <zhangyunh@gmail.com>
2026-02-18 18:46:02 +0800

    Fix bug in elf.c

--- library/elf/elf.c
+++ library/elf/elf.c
@@ -29,7 +29,7 @@ void elf_load(int pid, elf_reader reader, int argc, void** argv) {
         uint memsz        = pheader[i].p_memsz;
         uint filesz       = pheader[i].p_filesz;
         uint curr_pageno  = addr / PAGE_SIZE;
-        uint end_pageno   = (addr + memsz + PAGE_SIZE - 1) / PAGE_SIZE; // round up
+        uint end_pageno   = (addr + memsz) / PAGE_SIZE;
         uint curr_blockno = pheader[i].p_offset / BLOCK_SIZE;
         for (uint ppage_id, off = 0; off < filesz; off += BLOCK_SIZE) {
             /* Allocate one page (4KB) for every 8 blocks (512 bytes). */
@@ -44,7 +44,7 @@ void elf_load(int pid, elf_reader reader, int argc, void** argv) {
             memcpy(PAGE_ID_TO_ADDR(ppage_id) + (off % PAGE_SIZE), buf, size);
         }
 
-        while (curr_pageno < end_pageno) {
+        while (curr_pageno <= end_pageno) {
             uint ppage_id = earth->mmu_alloc();
             earth->mmu_map(pid, curr_pageno++, ppage_id);
             memset(PAGE_ID_TO_ADDR(ppage_id), 0, PAGE_SIZE);
